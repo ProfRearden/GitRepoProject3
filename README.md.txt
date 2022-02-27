cfac63eb178d67f5c9766bc2b4ca4de04ef007ad
unknown <sheena@brud.local>
2022-02-26 19:54:02 -0800

    test commandline arguments

---
+++ README.md.txt
@@ -0,0 +1,2 @@
+This will be test to notify users via slack and gmail.
+Later this will be used in Linear
---
+++ arg_test.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+# print('INCOMING FROM GITHUB: %s  %s' % (sys.argv[2], sys.argv[3:]))
+#
+# #print(str(sys.argv))
+#
+#print(sys.argv[1], *(sys.argv[2:]))
+stripped = [s.strip('\\r') for s in sys.argv[2:]]
+print(sys.argv[1], *stripped)
+
+
+#argument_2 = ' '.join(*(sys.argv[3:].strip("\r")).replace('\r','').split())
+# print(argument_2)
+
+
+# my_list = ['this\n', 'is\n', 'a\n', 'list\n', 'of\n', 'words\n']
+# stripped = [s.strip('\n') for s in my_list]
+# print(*stripped)
+
+#print(len(sys.argv))
+#run(sys.argv[1], (*(sys.argv[2:])))
+#run()
+#print('INCOMING FROM GITHUB: %s' % sys.argv[1])
+#run: python software_release_protocol.py ${{ github.event.release.name}}
+#run: python software_release_protocol.py ${{ github.event.release.tag_name}}
