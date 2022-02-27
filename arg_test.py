02c9d8778f1e5f229203b44bb707dd203336a2cf
ProfRearden <67852031+ProfRearden@users.noreply.github.com>
2022-02-26 21:30:41 -0800

    Update arg_test.py

--- arg_test.py
+++ arg_test.py
@@ -4,9 +4,9 @@ import sys
 
 # print('INCOMING FROM GITHUB: %s  %s' % (sys.argv[2], sys.argv[3:]))
 #
-# #print(str(sys.argv))
+print(sys.argv)
 #
-print(sys.argv[1], sys.argv[2:])
+#print(sys.argv[1], sys.argv[2:])
 stripped = [s.strip('\r') for s in sys.argv[2:]]
 print(sys.argv[1], *stripped)
 
