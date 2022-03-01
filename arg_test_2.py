761265fb8d37db822a2149fd13361c351fd31b77
ProfRearden <67852031+ProfRearden@users.noreply.github.com>
2022-02-28 18:27:00 -0800

    Update arg_test_2.py

--- arg_test_2.py
+++ arg_test_2.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 string_object = ''.join(sys.argv[2])
-stripped = string_object.replace('\\r'," ").replace("\\", " ").replace('\\n', " ")
+stripped = string_object.replace('\\r',"").replace('\\n','\n')
 print(sys.argv[1], stripped)
 #print(type(stripped))
