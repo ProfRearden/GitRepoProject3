cf48f38661cf0c8728d337f34a962d770cbaa8a9
ProfRearden <67852031+ProfRearden@users.noreply.github.com>
2022-02-28 18:17:59 -0800

    Update arg_test_2.py

--- arg_test_2.py
+++ arg_test_2.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-string_object = ''.join(sys.argv[2])
-stripped = string_object.replace('\r'," ").replace("\\", " ")
-print(sys.argv[1], stripped)
-print(type(stripped))
+#string_object = ''.join(sys.argv[2])
+#stripped = string_object.replace('\r'," ").replace("\\", " ")
+#print(sys.argv[1], stripped)
+#print(type(stripped))
+argument_2 = ''.join(*(sys.argv[2:].strip("\r")).replace('\r','').split())
+print(argument_2)
