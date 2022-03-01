f1d1947b342c18df7a89f33112cdeea6546fa53f
ProfRearden <67852031+ProfRearden@users.noreply.github.com>
2022-02-28 17:09:37 -0800

    Update arg_test.py

--- arg_test.py
+++ arg_test.py
@@ -7,8 +7,9 @@ import sys
 #print(sys.argv)
 #
 #print(sys.argv[1], sys.argv[2:])
-stripped = [s.strip('\r') for s in sys.argv[2:]]
-print(sys.argv[1], *stripped)
+#stripped = [s.strip('\r') for s in sys.argv[2:]]
+stripped = sys.argv[2:]).replace('\r'," ").replace("\\", " ")
+print(sys.argv[1], stripped)
 
 
 #argument_2 = ' '.join(*(sys.argv[3:].strip("\r")).replace('\r','').split())
