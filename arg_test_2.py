83964878d4c98cad378baeb7227db9aab9744cfb
ProfRearden <67852031+ProfRearden@users.noreply.github.com>
2022-03-01 16:48:23 -0800

    Update arg_test_2.py

--- arg_test_2.py
+++ arg_test_2.py
@@ -3,5 +3,7 @@ import sys
 
 string_object = ''.join(sys.argv[2])
 stripped = string_object.replace('\\r',"").replace('\\n','\n')
+pkg_name = os.environ['PACKAGE_NAME']
 print(sys.argv[1], stripped)
+print(pkg_name)
 #print(type(stripped))
